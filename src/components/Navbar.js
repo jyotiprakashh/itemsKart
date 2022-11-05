2ee70a8cf6d1829dc746da33fc79c93477bab17d
Jyoti Prakash Panda <jppanda12345@gmail.com>
2022-11-05 07:35:15 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    
+    render() { 
+        return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          HungryCart
+        </a>
+        </nav>
+        
+      
+      );
+    }
+}
+ 
+export default Navbar;
