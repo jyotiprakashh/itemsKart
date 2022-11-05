479706554495cf238de7d6f4a543b51cb30853c1
Jyoti Prakash Panda <jppanda12345@gmail.com>
2022-11-05 07:45:02 +0530

    Updates

---
+++ precache-manifest.396c4aee8734424b45ca83988d6ac95f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4faf792c61324dcb3bb",
+    "url": "/itemsKart/static/css/main.7085aec9.chunk.css"
+  },
+  {
+    "revision": "c4faf792c61324dcb3bb",
+    "url": "/itemsKart/static/js/main.c4faf792.chunk.js"
+  },
+  {
+    "revision": "4d5909f16dbfde8279fc",
+    "url": "/itemsKart/static/js/1.4d5909f1.chunk.js"
+  },
+  {
+    "revision": "2c6bf08842d49e29ec67",
+    "url": "/itemsKart/static/js/2.2c6bf088.chunk.js"
+  },
+  {
+    "revision": "96dc1e69009dfcada6f9",
+    "url": "/itemsKart/static/js/runtime~main.96dc1e69.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/itemsKart/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3a71bd013aad0e3f805037a110e150d5",
+    "url": "/itemsKart/index.html"
+  }
+];
